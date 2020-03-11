f4ad5839c12ec55411ea749558ccb515163990b9
Kourosh Delphani <delphani@gmail.com>
2020-03-11 12:59:14 +0330

    remove module

--- Cheque/src/main/java/com/dsa/cheque/ws/model/reponse/package-info.java
+++
@@ -1,7 +0,0 @@
-@XmlSchema(namespace = WsConfig.CHEQUE_SERVICE_SCHEMA_NAMESPACE, elementFormDefault = XmlNsForm.QUALIFIED)
-package com.dsa.cheque.ws.model.reponse;
-
-import com.dsa.cheque.config.WsConfig;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
